96998f7fc464812ba50588cdb5f29e7e5aba6063
Arshath <arshath12345@gmail.com>
2020-02-17 20:25:06 +0530

    Revert "Declaration of variables,arrays"
    This reverts commit 7a984dee154940f60fa8f3c9ccbed7a6357987e5.

--- Makefile
+++
@@ -1,5 +0,0 @@
-run:
-	lex tokens.l
-	bison parser.y
-	gcc parser.tab.c 
-	./a.out < test.c
--- test.c
+++ test.c
@@ -1,3 +1,14 @@
-int num;
-float arr[5],d;
-char **str,kls[20][20];
+int a = 10;
+
+for(int i=0;i<10.0;i++){
+    a++;
+}
+
+/*
+
+Hi. This is a multi line 
+comment. this can contain any chars.even /**
+
+*/
+
+// Single line comment
